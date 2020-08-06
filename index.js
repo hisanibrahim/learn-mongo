69fa08358fc426719e9348416bd3bf8642081250
Hisan Ibrahim <hisanibrahim@gmail.com>
2020-08-06 11:50:58 +0530

    create schema, compile to model and object based on the model

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ mongoose
   .connect("mongodb://localhost/playground")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB", err));
+
+// create schema
+const courseSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+});
+
+// complile schema to model that gives a class
+const Course = new mongoose.model("Course", courseSchema);
+
+// create an object based on model class
+const course = new Course({
+  name: "Introduction to common sense",
+  author: "Hisan",
+  tags: ["life", "knowledge"],
+  isPublished: true,
+});

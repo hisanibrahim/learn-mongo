e330eca16d34d3c0ee715e3a9c70189d067bd2ba
Hisan Ibrahim <hisanibrahim@gmail.com>
2020-08-07 15:00:44 +0530

    exercise 1

---
+++ exercise/solution.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost/mongo-exercises")
+  .then(() => console.log("Conected to mongo..."))
+  .catch((err) => console.error(err.message));
+
+const courseSchema = mongoose.Schema({
+  name: String,
+  author: String,
+  tags: [String],
+  date: { type: Date, default: Date.now },
+  isPublished: Boolean,
+  price: Number,
+});
+
+const Course = new mongoose.model("Course", courseSchema);
+
+async function getCourses() {
+  return await Course.find({
+    isPublished: true,
+    tags: { $in: ["backend", "frontend"] },
+  })
+    .sort("-price")
+    .select("name author price");
+}
+
+async function run() {
+  const courses = await getCourses();
+  console.log(courses);
+}
+
+run();

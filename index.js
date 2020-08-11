ba0fbe5af0d2a3f2df7f8fd5be2ecc59f8a88be5
Hisan Ibrahim <hisanibrahim@gmail.com>
2020-08-11 14:02:45 +0530

    validation in mongoose

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/playground")
+  .connect("mongodb://localhost/playground", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
 // create schema
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   author: String,
   price: Number,
   tags: [String],
@@ -15,21 +18,26 @@ const courseSchema = new mongoose.Schema({
   isPublished: Boolean,
 });
 
-// complile schema to model that gives a class
 const Course = new mongoose.model("Course", courseSchema);
 
 const createCourse = async () => {
-  // create an object based on model class
   const course = new Course({
-    name: "Mongo Bootcamp",
+    // name: "Mongo Bootcamp",
     author: "Hisan",
     price: 1500,
     tags: ["coding", "learn"],
     isPublished: false,
   });
 
-  const result = await course.save();
-  console.log(result);
+  try {
+    const result = await course.save();
+
+    // Another way to validate without saving data
+    // const result = await course.validate();
+    console.log(result);
+  } catch (err) {
+    console.error(err.message);
+  }
 };
 
 const getCourses = async (id) => {
@@ -66,7 +74,4 @@ const removeCourse = async (id) => {
   console.log(course);
 };
 
-// createCourse();
-// getCourses("5f2d2dd44136e63d30bd6759");
-// updateCourse("5f2d2dd44136e63d30bd6759");
-removeCourse("5f2d2dd44136e63d30bd6759");
+createCourse();
